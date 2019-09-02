f6953aa43728451a4990b4341b05b5c9cf3d8be0
Stuart Lang <stuart.b.lang@gmail.com>
2019-09-02 16:16:46 +0100

    Initial commit :tada:

---
+++ XamAppWithHeaders.iOS/AccessiblityTraitEffect.cs
@@ -0,0 +1,45 @@
+﻿using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using static XamAppWithHeaders.Accessibility;
+
+[assembly: ResolutionGroupName("XamAppWithHeaders")]
+[assembly: ExportEffect(typeof(XamAppWithHeaders.iOS.AccessibilityTraitEffect), "AccessibilityTraitEffect")]
+
+namespace XamAppWithHeaders.iOS
+{
+    public class AccessibilityTraitEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            AddAccessibilityTraits();
+        }
+
+        protected override void OnDetached()
+        {
+        }
+
+        protected override void OnElementPropertyChanged(System.ComponentModel.PropertyChangedEventArgs args)
+        {
+            if (args.PropertyName == AccessibilityTraitsProperty.PropertyName)
+            {
+                AddAccessibilityTraits();
+            }
+            else
+            {
+                base.OnElementPropertyChanged(args);
+            }
+        }
+
+        void AddAccessibilityTraits()
+        {
+            var traits = Control.AccessibilityTraits;
+
+            var newTraits = GetAccessibilityTraits(Element);
+
+            if ((newTraits & AccessibilityTrait.Header) > 0) traits |= UIAccessibilityTrait.Header;
+
+            Control.AccessibilityTraits = traits;
+        }
+    }
+}
---
+++ XamAppWithHeaders/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XamAppWithHeaders.Models;
+
+namespace XamAppWithHeaders.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
